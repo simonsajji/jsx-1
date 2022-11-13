230517d726c4c7c06524a98cabc8a44f52fa4457
simon <brooklinsimon29@gmail.com>
2022-11-13 17:52:03 +0530

    jsx-1 assignment 1

--- src/App.js
+++ src/App.js
@@ -4,20 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>About Us</h1>
+      <p>At Zappos.com, our purpose is Simple : to live and deliver WOW.</p>
+      <p>Twenty years ago we began as a small retailer that only sold shoes. Today we still sell shoes - as well as <br></br> clothing, handbags, accessories and more. The "more" is providing the best customer service, customer experience and <br/>company culture. We are showing the world that it is possible to simultaneously deliver<br/> happiness to  the customers, employees, vendors and community in long term sustainable way.</p>
+      <p>We hope that in the future people wont even realize that we started selling shoes online. Instead they will know Zappos as a <br/>service company that  sells ____</p>    
     </div>
   );
 }
